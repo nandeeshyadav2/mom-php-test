02176993fad9aa1995c66cf5819b7a4a05de620f
Nandeesh R <nandeesh.r@globallogic.com>
2021-03-03 19:09:25 +0530

    mvc

--- constants.php
+++ constants.php
@@ -4,5 +4,5 @@ define("to","nandeeshyadav2@gmail.com");
 define("Subject","testing");
 define("Host","smtp.gmail.com");
 define("Username","nandeeshyadav6@gmail.com");
-define("Password","Nandeesh@123");
+define("Password","****");
 define("Port",587);
